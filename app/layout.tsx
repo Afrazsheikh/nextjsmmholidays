df9eedaa17a1d7b1ee1ef2a9381a91d3248b0fff
Afraz Sheikh <sheikhafraz04@gmail.com>
2026-02-23 13:31:08 +0530

    added google anyltics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata = {
   title: "MM Holidays",
   description: "Best travel packages",
   verification: {
-    google: "jqdcvh2EUC0dPqq3naqGU2cy6HRVydIN1jbqotdqv8g", // paste from search console
+    google: "7Ud9nnwIsGAt5lMuV9_DSnD920yNNmwmhfCd_6wHYtY", 
   },
 };
 
